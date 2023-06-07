5ecce5f9c30e956bd958a35fd724fd4b0eef7d55
Tran Toan <tofbest267@gmail.com>
2023-06-07 21:13:19 +0700

    feat: fix using expression

--- src/pages/Quizzes/Quizzes.jsx
+++ src/pages/Quizzes/Quizzes.jsx
@@ -9,14 +9,25 @@ function Quizzes() {
   ]);
   const [isLoading, setIsLoading] = useState(true);
 
-  const URL = "https://opentdb.com/api.php?amount=5";
+  const URL = `https://opentdb.com/api.php?amount=5`;
 
   useEffect(() => {
     const fetchQuestionsData = async () => {
       try {
         const response = await fetch(URL);
         const data = await response.json();
-        setQuestionsData(data.results);
+        const formattedResults = data.results.map((result) => ({
+          ...result,
+          question: result.question.replace(/(&quot;|&#039;)/g, '"'),
+          correct_answer: result.correct_answer.replace(
+            /(&quot;|&#039;)/g,
+            '"'
+          ),
+          incorrect_answers: result.incorrect_answers.map((answer) =>
+            answer.replace(/(&quot;|&#039;)/g, "'")
+          ),
+        }));
+        setQuestionsData(formattedResults);
         setIsLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
